4452da90f315050ca0e70bb67fc7d8d672bc4b67
waves1423 <mercy.kitty3@gmail.com>
2022-07-07 05:42:57 +0900

    add:make AdminSeeder

--- resources/views/layouts/admin-navigation.blade.php
+++ resources/views/layouts/admin-navigation.blade.php
@@ -16,6 +16,11 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('admin.users.index')" :active="request()->routeIs('admin.users.index')">
+                        ユーザー一覧
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
