e12ee9080c308f0c0dde54db0c97f36386732ce2
waves1423 <mercy.kitty3@gmail.com>
2022-08-07 18:00:57 +0900

    add:logo

--- resources/views/layouts/admin-navigation.blade.php
+++ resources/views/layouts/admin-navigation.blade.php
@@ -4,7 +4,7 @@
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
-                <div class="shrink-0 flex items-center">
+                <div class="shrink-0 flex items-center w-60">
                     <a href="{{ route('admin.recipients.index') }}">
                         <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
                     </a>
