516a4e6266a75086109e786e32efdc850fe01687
waves1423 <mercy.kitty3@gmail.com>
2022-08-07 16:19:59 +0900

    add:logo

--- resources/views/layouts/user-navigation.blade.php
+++ resources/views/layouts/user-navigation.blade.php
@@ -1,10 +1,10 @@
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="max-w-7xl mx-auto px-4 py-2 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
-                <div class="shrink-0 flex items-center">
+                <div class="shrink-0 flex items-center w-60">
                     <a href="{{ route('user.recipients.index') }}">
                         <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
                     </a>
