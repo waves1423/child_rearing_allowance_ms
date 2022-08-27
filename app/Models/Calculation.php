ea1301281d810d2d9757ca210af7077229c2286d
waves1423 <mercy.kitty3@gmail.com>
2022-08-27 18:24:33 +0900

    add:move DB operation methods to model class

--- app/Models/Calculation.php
+++ app/Models/Calculation.php
@@ -2,8 +2,14 @@
 
 namespace App\Models;
 
+use App\Models\Deduction;
+use App\Models\Dependent;
+use App\Models\Income;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Throwable;
 
 class Calculation extends Model
 {
@@ -44,5 +50,71 @@ class Calculation extends Model
     public function deduction()
     {
         return $this->hasOne(Deduction::class);
+    }
+
+    public function storeCalculation($request, $id)
+    {
+        try{
+            DB::transaction(function () use($request, $id) {
+                $calculation = $this->create([
+                    'recipient_id' => $id,
+                    'deducted_income' => $this->getTotalIncome($request)
+                ]);
+                Dependent::create([
+                    'calculation_id' => $calculation->id,
+                    'total' => $request->total,
+                    'elder' => $request->elder,
+                    'special' => $request->special,
+                    'year_old_16to18' => $request->year_old_16to18,
+                    'other_child' => $request->other_child
+                ]);
+                Income::create([
+                    'calculation_id' => $calculation->id,
+                    'income' => $request->income,
+                    'type' => $request->type,
+                    'deducted_income' => $request->deducted_income,
+                    'support_payment' => $request->support_payment,
+                    'deducted_support_payment' => $request->deducted_support_payment
+                ]);
+                Deduction::create([
+                    'calculation_id' => $calculation->id,
+                    'disabled' => $request->disabled,
+                    'specially_disabled' => $request->specially_disabled,
+                    'singleparent_or_workingstudent' => $request->singleparent_or_workingstudent,
+                    'special_spouse' => $request->special_spouse,
+                    'medical_expense' => $request->medical_expense,
+                    'small_enterprise' => $request->small_enterprise,
+                    'other' => $request->other,
+                    'common' => 80000
+                ]);
+            }, 2);
+        }catch(Throwable $e){
+            Log::error($e);
+            throw $e;
+        }
+    }
+
+    public function getTotalIncome($request)
+    {
+        $total_income =
+        $request->deducted_income
+        +$request->deducted_support_payment;
+
+        $total_deduction =
+        $request->disabled * 270000
+        +$request->specially_disabled * 400000
+        +$request->singleparent_or_workingstudent * 270000
+        +$request->special_spouse
+        +$request->medical_expense
+        +$request->small_enterprise
+        +$request->other
+        +$request->common;
+        
+        $total_deducted_income = $total_income - $total_deduction;
+        if($total_deducted_income < 0){
+            $total_deducted_income = 0;
+        }
+
+        return $total_deducted_income;
     }
 }
