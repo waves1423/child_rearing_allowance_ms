2387d0f96bb5444eaa56e3858395997718111805
waves1423 <mercy.kitty3@gmail.com>
2022-10-07 23:12:07 +0900

    fix:recipient test name

--- tests/Feature/RecipientTest.php
+++ tests/Feature/RecipientTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class RecipientControllerTest extends TestCase
+class RecipientTest extends TestCase
 {
     use RefreshDatabase;
     /**
@@ -19,5 +19,10 @@ class RecipientControllerTest extends TestCase
         $response = $this->get('/recipients');
 
         $response->assertStatus(200)->assertViewIs('user.recipients.index');
+    }
+
+    public function index_special_recipients()
+    {
+        
     }
 }
