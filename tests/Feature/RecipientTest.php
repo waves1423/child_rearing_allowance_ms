28a841093d4dde259b0ca1e0eae32fe34f881f33
waves1423 <mercy.kitty3@gmail.com>
2022-10-10 07:44:36 +0900

    add:change from RefreshDatabase to DatabaseMigrations

--- tests/Feature/RecipientTest.php
+++ tests/Feature/RecipientTest.php
@@ -2,13 +2,15 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class RecipientTest extends TestCase
 {
-    use RefreshDatabase;
+    // use RefreshDatabase;
+    use DatabaseMigrations;
 
     /** @test */
     public function 児童扶養手当受給者一覧画面が表示される_ログインなし()
@@ -38,6 +40,18 @@ class RecipientTest extends TestCase
         $response = $this->get('/recipients/1');
 
         $response->assertStatus(200)
-            ->assertSee('島原　一子');
+            ->assertViewIs('user.recipients.show')
+            ->assertSee('受給者詳細：島原　一子');
+    }
+
+    /** @test */
+    public function 受給者の基本情報編集画面が表示される_ログインなし()
+    {
+        $this->artisan('db:seed', ['--class' => 'RecipientSeeder']);
+
+        $response = $this->get('/recipients/1/edit');
+
+        $response->assertStatus(200)
+            ->assertViewIs('user.recipients.edit');
     }
 }
