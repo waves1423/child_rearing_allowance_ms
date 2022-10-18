c814114c2d135bf09bb5eb0ce751631d1442ab37
waves1423 <mercy.kitty3@gmail.com>
2022-10-18 23:53:12 +0900

    add:setUp method for spouse test

--- tests/Feature/SpouseControllerTest.php
+++ tests/Feature/SpouseControllerTest.php
@@ -2,21 +2,29 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Spouse;
+use App\Models\User;
 
 class SpouseControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    use DatabaseMigrations;
 
-        $response->assertStatus(200);
+    public function setUp() :void
+    {
+        parent::setUp();
+        $this->artisan('db:seed', ['--class' => 'RecipientSeeder']);
+        $this->artisan('db:seed', ['--class' => 'SpouseSeeder']);
+
+        $this->user = User::factory()->create();
+        $this->spouse = Spouse::factory()->create();
+
+        $this->requestData = ([
+            'recipient_id' => $this->spouse->recipient_id,
+            'name' => $this->spouse->name,
+            'family_relationship' => $this->spouse->family_relationship
+        ]);
     }
 }
