ee3de9ae55e7b3967164a3dae16d5d27947b0569
waves1423 <mercy.kitty3@gmail.com>
2022-10-18 23:47:18 +0900

    add:setUp method for obligor test

--- tests/Feature/ObliogrControllerTest.php
+++ tests/Feature/ObliogrControllerTest.php
@@ -2,21 +2,29 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Obligor;
+use App\Models\User;
 
 class ObliogrControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    use DatabaseMigrations;
 
-        $response->assertStatus(200);
+    public function setUp() :void
+    {
+        parent::setUp();
+        $this->artisan('db:seed', ['--class' => 'RecipientSeeder']);
+        $this->artisan('db:seed', ['--class' => 'ObligorSeeder']);
+
+        $this->user = User::factory()->create();
+        $this->obligor = Obligor::factory()->create();
+
+        $this->requestData = ([
+            'recipient_id' => $this->obligor->recipient_id,
+            'name' => $this->obligor->name,
+            'family_relationship' => $this->obligor->family_relationship
+        ]);
     }
 }
