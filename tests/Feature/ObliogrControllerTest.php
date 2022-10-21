8f24f85df2b615bfa84c3fd45cbcd72c1eef4eaa
waves1423 <mercy.kitty3@gmail.com>
2022-10-22 00:13:47 +0900

    add:edit and create methods without logged-in

--- tests/Feature/ObliogrControllerTest.php
+++ tests/Feature/ObliogrControllerTest.php
@@ -27,4 +27,46 @@ class ObliogrControllerTest extends TestCase
             'family_relationship' => $this->obligor->family_relationship
         ]);
     }
+
+    //ゲスト用テスト（ログインなし）
+    /** @test */
+    public function 受給者情報詳細画面に扶養義務者情報が表示される_ログインなし()
+    {
+        $response = $this->get('/recipients/1');
+
+        $response->assertStatus(200)
+            ->assertViewIs('user.recipients.show')
+            ->assertSee('島原　和男');
+    }
+
+    /** @test */
+    public function 扶養義務者情報編集画面が表示される()
+    {
+        $response = $this->get('recipients/1/obligors/1/edit');
+
+        $response->assertStatus(200)
+            ->assertViewIs('user.obligors.edit')
+            ->assertSee('扶養義務者情報編集：島原　和男');
+    }
+
+    /** @test */
+    public function 扶養義務者情報を更新しようとしたときログイン画面に遷移する_ログインなし()
+    {
+        $response = $this->put('recipients/1/obligors/1',
+        [
+            'recipient_id' => 1,
+            'name' => '島原　一雄',
+            'family_relationship' => '父'
+        ]);
+
+        $response->assertRedirect('/login');
+    }
+
+    /** @test */
+    public function 扶養義務者を新規登録しようとしたときログイン画面に繊維する_ログインなし()
+    {
+        $response = $this->post('/recipients/2/obligors', $this->requestData);
+
+        $response->assertRedirect('/login');
+    }
 }
