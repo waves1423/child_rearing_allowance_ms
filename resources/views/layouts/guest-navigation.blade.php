db2bd6be9070402b1af1018ac12b42d3a79cb58b
waves1423 <mercy.kitty3@gmail.com>
2022-11-19 07:53:44 +0900

    fix:adjust routing, add:view for download button

--- resources/views/layouts/guest-navigation.blade.php
+++ resources/views/layouts/guest-navigation.blade.php
@@ -22,7 +22,7 @@
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('user.functions')" :active="request()->routeIs('user.functions.index')">
+                    <x-nav-link :href="route('user.functions.index')" :active="request()->routeIs('user.functions.index')">
                         各種機能
                     </x-nav-link>
                 </div>
