1926f79b0c44d04ee7e68ba9a69bc08643cc2804
waves1423 <mercy.kitty3@gmail.com>
2022-11-18 23:26:31 +0900

    add:constructor

--- app/Http/Controllers/User/FunctionController.php
+++ app/Http/Controllers/User/FunctionController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Recipient;
 
 class FunctionController extends Controller
 {
+    public function __construct(Recipient $recipient)
+    {
+        $this->recipient = $recipient;
+    }
     public function index()
     {
         return view('user.functions.index');
